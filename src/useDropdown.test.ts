6a54c191a2a5c5cc9af503272a745595b413e984
eden lane <edencore@gmail.com>
2020-06-26 20:19:07 +0300

    added tests for mouse leave and input focus

--- src/useDropdown.test.ts
+++ src/useDropdown.test.ts
@@ -97,17 +97,29 @@ describe('Mouse events', () => {
     li.dataset.index = itemProps['data-index'].toString();
 
     act(() => {
-      const event = new MouseEvent('mouseenter', {
-        view: window,
-        bubbles: true,
-        cancelable: true,
-      });
-
+      const event = new MouseEvent('mouseenter');
       li.dispatchEvent(event);
     })
 
     const {highlightedIndex} = result.current;
     expect(highlightedIndex).toBe(2);
+  });
+
+  test('should open menu on input focus', () => {
+    const {result} = renderHook(() => useDropdown({onSelect, items}));
+    const {getInputProps} = result.current;
+    const inputProps = getInputProps();
+    const input = document.createElement('input');
+    input.addEventListener('focus', inputProps.onFocus);
+
+    expect(result.current.isOpen).toBe(false);
+
+    act(() => {
+      const event = new Event('focus');
+      input.dispatchEvent(event);
+    });
+
+    expect(result.current.isOpen).toBe(true);
   })
 })
 
@@ -130,6 +142,24 @@ describe('reducer', () => {
       highlightedIndex: 2,
       type:  StateChangeType.SET_HIGHLIGHTED_INDEX,
     });
+  })
 
+  test('should call reducer with new index on mouse leave', () => {
+    const reducer = jest.fn((state, action) => state);
+    const {result} = renderHook(() => useDropdown({onSelect, items, reducer}));
+    const {getMenuProps} = result.current;
+    const menuProps = getMenuProps();
+    const ul = document.createElement('ul');
+    ul.addEventListener('mouseleave', menuProps.onMouseLeave);
+
+    act(() => {
+      const event = new MouseEvent('mouseleave');
+      ul.dispatchEvent(event);
+    });
+
+    expect(reducer).toHaveBeenCalledWith(expect.anything(), {
+      highlightedIndex: -1,
+      type:  StateChangeType.SET_HIGHLIGHTED_INDEX,
+    });
   })
 })
